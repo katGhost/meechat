b39789b54452ddca4f985057c043ec64f04e00fc
katGhost <katghost124@gmail.com>
2026-01-28 16:53:59 +0200

    install python-dotenv, configure app

--- app.py
+++ app.py
@@ -7,16 +7,46 @@ from string import ascii_uppercase, ascii_lowercase
 
 from flask import Flask, flash, jsonify, redirect, render_template, request  
 from flask_socketio import SocketIO, emit, send, join_room, leave_room
+from werkzeug.middleware.proxy_fix import ProxyFix
 
 
 
+basedir = os.path.abspath(os.path.dirname(__file__))
+# config class
+class Config:
+    SECRET_KEY = os.environ.get("SECRET_KEY", "dev-secret-key")
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL") or \
+        f"sqlite:///{os.path.join(basedir, 'app.db')}"
+    CORS_ORIGIN = os.getenv("CORS_ORIGIN", "*")
+    DEBUG = True
+
+    # Rooms
+    chat_rooms = [
+        "General",
+        "Software Development",
+        "Showoff WIP",
+        "Funny Stuff"
+    ]
+
+
+# Configure flask app
 app = Flask(__name__)
+app.config.from_object(Config)
 
-app.secret_key = "M0jsib73-009naIne-8nwTjm3"
-app.config['SESSION_PERMANENT'] = False
-app.config['SESSION_TYPE'] = "filesystem"
-# socket
-socket = SocketIO(app, cors_allowed_origins="*")
+# handle reserve proxy
+app.wsgi_app = ProxyFix(app.wsgi_app, x_proto=1, x_host=1)
+
+# socket setup
+socket = SocketIO(
+    app,
+    cors_allowed_origins=app.config["CORS_ORIGIN"],
+    logger=True,
+    engineio_logger=True
+)
+
+# dictionary to hold users
+active_users: Dict[str, dict] = {}
 
 
 @app.route('/', methods=['POST', 'GET'])
@@ -25,5 +55,5 @@ def index():
 
 
 if __name__ == '__main__':
-    socket.run(app, debug=True, host='0.0.0.0')
+    socket.run(app, debug=app.config["DEBUG"])
 
