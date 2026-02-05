94a6671e748848b4a6a207c5acf2409485ebf81d
katGhost <katghost124@gmail.com>
2026-02-05 11:04:06 +0200

    Initialize packages

--- app/extensions.py
+++ app/extensions.py
@@ -1,6 +1,10 @@
 from flask_socketio import SocketIO
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_migrate import Migrate
 
 
 db = SQLAlchemy()
 socketio = SocketIO()
+bcrypt = Bcrypt()
+migrate = Migrate()
