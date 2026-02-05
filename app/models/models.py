1a0889a130b9931e20a2e57430be7b8211458f7d
katGhost <katghost124@gmail.com>
2026-02-05 11:02:25 +0200

    add rooms and messages models

--- app/models/models.py
+++ app/models/models.py
@@ -35,7 +35,7 @@ class Message(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     body = db.Column(db.Text, nullable=False)
-    timestamp = db.Column(db.DateTime, default=datetime.utcnow, index=True)
+    timestamp = db.Column(db.DateTime, default=datetime.now(), index=True)
 
     user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
     room_id = db.Column(db.Integer, db.ForeignKey("rooms.id"), nullable=False)
